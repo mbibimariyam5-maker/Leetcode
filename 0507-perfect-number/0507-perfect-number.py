e000872fb7ae8be2c1bfb712b2b4b923b2bbc0eb
M.Bibi Mariyam <mbibimariyam5@gmail.com>
2026-09-23 22:01:32 +0530

    Time: 3 ms (92.02%), Space: 12.7 MB (9.24%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,17 @@
+import math
+
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        if num <= 1:
+            return False
+
+        sum = 1
+
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                sum += i
+
+                if i != num // i:
+                    sum += num // i
+
+        return sum == num
